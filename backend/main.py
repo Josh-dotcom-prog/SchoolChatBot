c66293f6a70aa605ad5ecef208a886488671e238
Josh-dotcom-prog <edyangujoshua@gmail.com>
2025-05-20 13:38:05 +0300

    paul is a genius

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import  HTMLResponse
 from .chatbot import get_response
+from fastapi.templating import Jinja2Templates  
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
 
 # Enable CORS for frontend
 app.add_middleware(
@@ -19,3 +23,12 @@ async def chat(request: Request):
     user_message = body.get("message")
     bot_response = get_response(user_message)
     return {"response": bot_response}
+
+
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html"
+    )
